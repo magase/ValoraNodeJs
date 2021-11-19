1a04d732ab1965a117ff2a5b596c5570d1e655a6
magase <magaseHpDesktop@users.noreply.github.com>
2021-11-19 14:35:51 +0100

    Cambiar conexion con la BBDD
    Cambiar conexion con la BBDD

---
+++ src/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+    database: {
+        //Crear la conextion
+            host: 'localhost',
+            user: 'root',
+            password: '',
+            database: 'valora'
+    }
+}
