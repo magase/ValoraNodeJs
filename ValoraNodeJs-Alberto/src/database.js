1b568e8cb93bf7d2430a96e346416eba4285353b
davzamber <davidzb2@icloud.com>
2021-11-30 10:07:12 +0100

    update

---
+++ ValoraNodeJs-Alberto/src/database.js
@@ -0,0 +1,72 @@
+const mysql = require("mysql");
+const { promisify } = require('util')
+const { database } = require("./keys");
+
+const pool = mysql.createPool(database);
+
+pool.getConnection((err, connection) => {
+  if (err) {
+    if (err.code === "PROTOCOL_CONNECTION_LOST") {
+      console.error("La conexion con la base de datos se ha cerrado");
+    }
+    if (err.code === "ER_CON_COUNT_ERROR") {
+      console.error("La base de datos tiene demasiadas conexiones");
+    }
+    if (err.code === "ECONNREFUSED") {
+      console.error("La conexion con la base de datos ha sido rechazada");
+    }
+  }
+
+  if(connection) connection.release()
+    console.log('DB está conectada')
+  return;
+  
+});
+//Promesas
+pool.query = promisify(pool.query);
+
+module.exports = pool
+
+/** 
+//Crear la conextion
+const conector = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "valora",
+});
+
+const conectar = () => {
+  conector.connect((err) => {
+    if (err) throw err;
+    console.log("Conectado");
+  });
+};
+
+const agregarUser = (email, password, nombreUsuario, categoriaUsuario) => {
+  const sql = `INSERT INTO tbl_usuarios (id, email, password, nombre_usuario, categoria_usuario) values (${null}, "${email}", "${password}", "${nombreUsuario}", "${categoriaUsuario}")`;
+  conector.query(sql, function (err, result, filed) {
+    if (err) throw err;
+    console.log(result);
+  });
+};
+
+const agregarIncidencia = (
+  usuario_creador,
+  usuario_asignado,
+  estado_incidencia,
+  categoria_incidencia,
+  fecha_creacion,
+  fecha_final,
+  nombre_incidencia,
+  descripcion
+) => {
+  const sql = `INSERT INTO tbl_incidencias (id,usuario_creador,usuario_asignado,estado_incidencia,categoria_incidencia,fecha_creacion,fecha_final,nombre_incidencia,descripcion) values (${null},"${usuario_creador}","${usuario_asignado}","${estado_incidencia}","${categoria_incidencia}","${fecha_creacion}","${fecha_final}","${nombre_incidencia}","${descripcion}")`;
+  conector.query(sql, function (err, result, filed) {
+    if (err) throw err;
+    console.log(result);
+  });
+};
+
+export { conectar, agregarUser, agregarIncidencia };
+*/
---
+++ ValoraNodeJs-Alberto/src/lib/passport.js
@@ -0,0 +1,51 @@
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+const pool = require('../database')
+const helpers = require('../lib/helpers')
+
+passport.use('local.signin', new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password',
+    passReqToCallback: true
+  }, async (req, email, password, done) => {
+      console.log(req.body);
+    const rows = await pool.query('SELECT * FROM tbl_usuarios WHERE email = ?', [email]);
+    if (rows.length > 0) {
+      const user = rows[0];
+      const validPassword = await helpers.matchPassword(password, user.password)
+      if (validPassword) {
+        done(null, user, req.flash('success', 'Bienvenido ' + user.nombre_usuario));
+      } else {
+        done(null, false, req.flash('message', 'Contraseña incorrecta'));
+      }
+    } else {
+      return done(null, false, req.flash('message', 'El usuario no existe'));
+    }
+  }));
+
+passport.use('local.signup', new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password',
+    passReqToCallback: true
+}, async(req, email, password, done)=>{
+    const {nombre_usuario} = req.body
+    const newUser = {
+        email,
+        password,
+        nombre_usuario
+    };
+    newUser.password = await helpers.encryptPassword(password);
+    const result = await pool.query('INSERT INTO tbl_usuarios set ?', [newUser]);
+    newUser.id = result.insertId;
+    return done(null, newUser);
+
+}))
+
+passport.serializeUser((user, done) => {
+    done(null, user.id)
+})
+
+passport.deserializeUser(async(id, done) =>{
+    const rows = await pool.query('SELECT * FROM tbl_usuarios WHERE id = ?', [id])
+    done(null, rows[0]);
+})
