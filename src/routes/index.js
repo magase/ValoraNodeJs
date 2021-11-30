797a59f91e624024aff7fb06922a60f5fb18b475
manuelgarcia <magase98@gmail.com>
2021-11-30 21:01:22 +0100

    Index principal cabio nombres
    Index principal cabio nombres

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,9 @@
 const express = require('express') 
 const router = express.Router()
+const { isLoggedIn, isNotLoggedIn} = require('../lib/auth');
 
-//const pool = require('../database');
-// router.get('/', (req, res) =>{
-//     res.render('auth/signin');
-// })
+router.get('/signin', isNotLoggedIn, (req, res) => {
+    res.render('auth/signin');
+})
 
 module.exports = router
